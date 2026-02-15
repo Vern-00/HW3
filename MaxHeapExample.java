bb29ce8b69aebd1cb6c52dafe836c858550421e9
Vern-00 <verntoor23@gmail.com>
2026-02-14 22:24:44 -0800

    Inital Commit

---
+++ MaxHeapExample.java
@@ -0,0 +1,15 @@
+class WordFreq {
+    String word;
+    int frequency;
+
+    WordFreq(String word, int frequency) {
+        this.word = word;
+        this.frequency = frequency;
+    }
+}
+
+public class MaxHeapExample {
+    public static void main(String[] args) {
+        
+    }
+}
