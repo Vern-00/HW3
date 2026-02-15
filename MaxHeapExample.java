2e05a4666672484a41d7fabb58eb6612d597ea0b
Vern-00 <verntoor23@gmail.com>
2026-02-14 22:52:35 -0800

    created WordFreq array based on given instructions

--- MaxHeapExample.java
+++ MaxHeapExample.java
@@ -38,6 +38,19 @@ public class MaxHeapExample {
     }
 
     public static void main(String[] args) {
+        WordFreq[] data = {
+            new WordFreq("happy", 400),
+            new WordFreq("satisfied", 100),
+            new WordFreq("neutral", 300),
+            new WordFreq("would buy again", 200),
+            new WordFreq("terrible", 160),
+            new WordFreq("inconvenient", 900),
+            new WordFreq("difficult to use", 100),
+            new WordFreq("easy to use", 140),
+            new WordFreq("would recommend to friends", 800),
+            new WordFreq("visit the store", 700)
+        };
 
+        buildMaxHeap(data);
     }
 }
